ae9113327a4e79bdfa1f7c72f64b5f2401c70058
Sebastian Buchner <sebbel9@freenet.de>
2026-03-29 18:32:46 +0200

    added german names for pokemon name

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/bulbasaur'
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/squirtle'
 
-fetch(BASE_URL)
-.then(response => {
-    return response.json();
-})
-.then(pokemon => {
-    const pic = document.querySelector(".picture")
-    pic.setAttribute("src", pokemon.sprites.front_default)
+async function getPokemon(BASE_URL) {
+    try {
+        const response = await fetch(BASE_URL)
+        const pokemon = await response.json()
 
-    const name = document.querySelector(".name")
-    name.innerText = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+        const pic = document.querySelector(".picture")
+        pic.setAttribute("src", pokemon.sprites.front_default)
 
-    const id = document.querySelector(".id")
-    id.innerText = "Nr. " + pokemon.id
+        const id = document.querySelector(".id")
+        id.textContent = (`Nr. ${pokemon.id}`)
 
-    for (let type of pokemon.types) {
-        const typesElement = document.createElement("p")
-        typesElement.classList.add("types")
-        typesElement.innerText = type.type.name[0].toUpperCase() + type.type.name.slice(1);
-        document.querySelector(".types-container").appendChild(typesElement);
-}   
-})
+        const speciesResponse = await fetch(pokemon.species.url)
+        const species = await speciesResponse.json()
+        const germanName = species.names.find(german => german.language.name === "de")
+        const name = document.querySelector(".name")
+        name.textContent = germanName.name[0].toUpperCase() + germanName.name.slice(1);
 
-.catch(error => console.log(error))
+        // const name = document.querySelector(".name")
+        // name.textContent = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
 
+        for (const type of pokemon.types) {
+            let typesElement = document.createElement("p")
+            typesElement.classList.add(type.type.name)
+            typesElement.textContent = type.type.name[0].toUpperCase() + type.type.name.slice(1);
+            document.querySelector(".types-container").append(typesElement)
+        }
 
-
-
-// Pokémon-Array durchgehen
-// Für jeden Typ ein <span> erzeugen
-// Text setzen → Typname
-// Klasse setzen → Typname
-// CSS regelt Farbe, Abstand, Größe
-
-
-for (let type of pokemon.types) {
-    const typesElement = document.createElement("p")
-    typesElement.classList.add("types")
-    typesElement.innerText = type.type.name[0].toUpperCase() + type.type.name.slice(1);
-    document.querySelector(".types-container").appendChild(typesElement);
+    } catch(error){
+        console.log(error);
+    }
 }
+
+getPokemon(BASE_URL)
+
+
+/*
+1. BASE_URL so https://pokeapi.co/api/v2/pokemon verwenden um kein spezifisches Pokemon vorab zu wählen. Dann kann ich alle Pokemon laden, der Reihe nach.
+2. Limit auf 10 Stück festlegen und über Button (in HTML definieren) weiter 10 Stück laden können.
+ */
