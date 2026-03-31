02c68f3fcb0940a7b0e466eeac0b983fdf814847
Sebastian Buchner <sebbel9@freenet.de>
2026-03-31 22:08:32 +0200

    added template in html, css and js

--- index.js
+++ index.js
@@ -2,30 +2,48 @@ const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/squirtle'
 
 async function getPokemon(BASE_URL) {
     try {
-        const response = await fetch(BASE_URL)
-        const pokemon = await response.json()
+        const response = await fetch(BASE_URL);
+        const pokemon = await response.json();
 
-        const pic = document.querySelector(".picture")
-        pic.setAttribute("src", pokemon.sprites.front_default)
-
-        const id = document.querySelector(".id")
-        id.textContent = (`Nr. ${pokemon.id}`)
-
-        const speciesResponse = await fetch(pokemon.species.url)
-        const species = await speciesResponse.json()
-        const germanName = species.names.find(german => german.language.name === "de")
-        const name = document.querySelector(".name")
-        name.textContent = germanName.name[0].toUpperCase() + germanName.name.slice(1);
-
-        // const name = document.querySelector(".name")
-        // name.textContent = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+        const container = document.getElementById("pokecard");
+        const tmpl = document
+            .getElementById("pokecard_template")
+            .content.cloneNode(true);
+        tmpl.querySelector(".picture").setAttribute("src", pokemon.sprites.front_default)
+        tmpl.querySelector(".id").textContent = (`Nr. ${pokemon.id}`)
+        
+        const speciesResponse = await fetch(pokemon.species.url);
+        const species = await speciesResponse.json();
+        const germanName = species.names.find(german => german.language.name === "de");
+        tmpl.querySelector(".name").textContent = germanName.name[0].toUpperCase() + germanName.name.slice(1);
 
         for (const type of pokemon.types) {
-            let typesElement = document.createElement("p")
-            typesElement.classList.add(type.type.name)
-            typesElement.textContent = type.type.name[0].toUpperCase() + type.type.name.slice(1);
-            document.querySelector(".types-container").append(typesElement)
-        }
+            const typeElement = document.createElement("p")
+            typeElement.classList.add(type.type.name)
+            typeElement.textContent = type.type.name[0].toUpperCase() + type.type.name.slice(1)
+            tmpl.querySelector(".types-container").appendChild(typeElement)
+        };
+
+        container.appendChild(tmpl)
+        
+        // const pic = document.querySelector(".picture");
+        // pic.setAttribute("src", pokemon.sprites.front_default);
+
+        // const id = document.querySelector(".id");
+        // id.textContent = (`Nr. ${pokemon.id}`);
+
+        // const speciesResponse = await fetch(pokemon.species.url);
+        // const species = await speciesResponse.json();
+        // const germanName = species.names.find(german => german.language.name === "de");
+        // const name = document.querySelector(".name");
+        // name.textContent = germanName.name[0].toUpperCase() + germanName.name.slice(1);
+
+        // for (const type of pokemon.types) {
+        //     let typesElement = document.createElement("p")
+        //     typesElement.classList.add(type.type.name)
+        //     typesElement.textContent = type.type.name[0].toUpperCase() + type.type.name.slice(1);
+        //     document.querySelector(".types-container").append(typesElement)
+        // };
 
     } catch(error){
         console.log(error);
